27da9a7aeac267f71863d17961a872ded4d43408
Johan Scholten <johan8@gmail.com>
2012-02-02 01:19:45 +0100

    Loosing the location double check

--- conquer_club_-_graphs.user.js
+++ conquer_club_-_graphs.user.js
@@ -19,20 +19,18 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
 
 /*************** Actual code of the addon *******************/
 
-if (window.location.toString().indexOf('memberlist.php?mode=viewprofile')) {
-  	var root = $("div.panel.bg1").eq(0);
-	$("<input type='button' value='Create graph'/>").click(initGraphFromScreen).appendTo(root);
-	var players = []; // will contain the data
-	var waitMessage = $("<div id='waitForGraph'></div>").appendTo(root);
-	var tooltip = $('<div id="tooltip"></div>').css({
-		position: 'absolute',
-		display: 'none',
-		border: '1px solid #fdd',
-		padding: '2px',
-		'background-color': '#fee',
-		opacity: 0.80
-	}).appendTo("body");
-}
+var root = $("div.panel.bg1").eq(0);
+$("<input type='button' value='Create graph'/>").click(initGraphFromScreen).appendTo(root);
+var players = []; // will contain the data
+var waitMessage = $("<div id='waitForGraph'></div>").appendTo(root);
+var tooltip = $('<div id="tooltip"></div>').css({
+	position: 'absolute',
+	display: 'none',
+	border: '1px solid #fdd',
+	padding: '2px',
+	'background-color': '#fee',
+	opacity: 0.80
+}).appendTo("body");
 
 function initGraphFromScreen() {
 	$(this).remove(); // remove button
